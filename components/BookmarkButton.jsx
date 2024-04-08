1c83af88df12b451538756c7df31f638831e3134
tLiedts <123934476+tLiedts@users.noreply.github.com>
2024-04-08 18:37:11 +0200

    Cute the contact area on components

---
+++ components/BookmarkButton.jsx
@@ -0,0 +1,15 @@
+// Libs
+import React from 'react';
+
+// Components
+import { FaBookmark } from 'react-icons/fa';
+
+const BookmarkButton = ({ property }) => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+      <FaBookmark className="mr-2" /> Bookmark Property
+    </button>
+  );
+};
+
+export default BookmarkButton;
