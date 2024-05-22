723f4eb324d1e6a5002ca37927c0f58047b7d1ff
tLiedts <123934476+tLiedts@users.noreply.github.com>
2024-05-22 18:28:26 +0200

    add pagination on properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,18 +2,10 @@
 import React from 'react';
 
 // Components
-import PropertyCard from '@/components/PropertyCard';
 import PropertySearchForm from '@/components/PropertySearchForm';
+import Properties from '@/components/Properties';
 
-// Utils
-import { fetchProperties } from '@/utils/request';
-
-const PropertiesPage = async () => {
-  const properties = await fetchProperties();
-
-  // Sort properties by date
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
+const PropertiesPage = () => {
   return (
     <>
       <section className="bg-blue-700 py-4">
@@ -21,19 +13,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />{' '}
         </div>
       </section>
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map(property => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
---
+++ components/Properties.jsx
@@ -0,0 +1,67 @@
+'use client';
+// Libs
+import React, { useState, useEffect } from 'react';
+
+// Components
+import PropertyCard from '@/components/PropertyCard';
+import Spinner from '@/components/Spinner';
+import Pagination from '@/components/Pagination';
+
+const Properties = () => {
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(3);
+  const [totalItems, setTotalItems] = useState(0);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties?page=${page}&pageSize=${pageSize}`
+        );
+        if (!res.ok) {
+          throw new Error('failed to fetch data');
+        }
+        const data = await res.json();
+        setProperties(data.properties);
+        setTotalItems(data.total);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProperties();
+  }, [page, pageSize]);
+
+  const handlePageChange = newPage => {
+    setPage(newPage);
+  };
+
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map(property => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+        <Pagination
+          page={page}
+          pageSize={pageSize}
+          totalItems={totalItems}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Properties;
